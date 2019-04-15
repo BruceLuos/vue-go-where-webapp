9b7f820ab579b0e9762cd5be690938526a726446
brucluo <295448382@qq.com>
2019-04-15 18:48:35 +0800

    'vue'

---
+++ src/home/components/Swiper.vue
@@ -0,0 +1,51 @@
+<template>
+<div class="wrapper">
+  <swiper :options="swiperOption">
+    <!-- slides -->
+  <swiper-slide v-for="item of swiperList" :key="item.id">
+      <img class="swipe-img" :src="item.imgUrl"/>
+  </swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'HomeSwiper',
+    data : function(){
+        return{
+            swiperOption:{
+                pagination: '.swiper-pagination',
+                loop: true
+            },
+            swiperList:[{
+                id: '0001',
+                imgUrl: "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/d7bbc21db442366a882e04ddc984669a.jpg_750x200_85e640d9.jpg"
+            },{
+                id: '0002',
+                imgUrl: "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/389d0194766566f8f1185aa19b759c25.jpg_750x200_22620261.jpg"
+            },{
+                id: '0003',
+                imgUrl: "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20194/e5104d77fbfdb522bd4fe4f0451e517f.jpg_750x200_7d44f64b.jpg"
+            }
+            ]
+
+        }
+    }    
+}
+</script>
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+.wrapper
+    overflow: hidden
+    width: 100%
+    height : 0
+    padding-bottom: 31.25%
+    // 宽高比31.25
+    .swipe-img
+        width: 100%;
+
+</style>
