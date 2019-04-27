e2ea0936e31a04e57575b7b142a9da57477eb39e
brucluo <295448382@qq.com>
2019-04-28 00:01:15 +0800

    'v'

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '../home/Home.vue'
-import City from '../city/City.vue'
+import Home from '.../home/Home.vue'
+import City from '.../city/City.vue'
 
 Vue.use(Router)
 
