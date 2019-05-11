a6d9dd928fefd455af67b60a4b7d26dc7ba1d956
brucluo <295448382@qq.com>
2019-05-11 21:28:22 +0800

    'header-detail'

---
+++ src/pages/detail/components/Header.vue
@@ -0,0 +1,94 @@
+<template>
+<div>
+    <router-link 
+    tag="div" 
+    to="/" 
+    class="header-abs"
+    v-show="showAbs"
+    >
+        <div class="iconfont header-icon">&#xe606;</div>
+    </router-link>
+<div 
+class="header-fixed" 
+v-show="!showAbs"
+:style="opacityStyle"
+>
+    <router-link to="/" >
+    <div class="iconfont header-fixed-back">&#xe606;</div>
+    </router-link> 景点详情
+</div>
+</div>
+</template>
+
+<script>
+
+export default {
+    name: 'DetailHeader',
+    data() {
+        return {
+            showAbs: true,
+            opacityStyle: {
+                // 隐形范围0-1
+                opacity: 0
+            }
+        }
+    },
+    methods: {
+        handleScroll () {
+            //实现下拉时的渐隐渐现效果
+            const top = document.documentElement.scrollTop
+            if (top>60) {
+                let opacity = top/140
+                opacity = opacity > 1 ? 1:opacity
+                this.opacityStyle = { opacity }
+                // console.log(opacity)
+                this.showAbs = false
+            } else {
+                this.showAbs = true
+            }
+            
+        }
+    },
+    activated() {
+        window.addEventListener('scroll', this.handleScroll)
+    }
+    
+}
+</script>
+<style lang="stylus" scoped>
+@import '~@/assets/styles/varibles.styl'
+.header-abs
+    position absolute 
+    left .2rem
+    top .2rem
+    width .72rem
+    height .72rem
+    line-height .72rem
+    border-radius :.36rem
+    text-align center
+    background rgba(0,0,0, .8)
+    .header-icon
+        color #fff
+        font-size .5rem
+.header-fixed
+    position: fixed
+    top 0
+    left  0
+    right 0
+    overflow: hidden
+    background: $bgColor
+    height: 0.86rem
+    line-height: 0.86rem
+    text-align: center
+    color: #fff
+    font-size: .32rem
+    .header-fixed-back
+        position: absolute
+        top: 0
+        left: 0
+        // float: left
+        text-align: center
+        font-size: 0.5rem
+        width: 0.8rem
+        color: #fff
+</style>
